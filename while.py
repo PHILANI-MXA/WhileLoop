0a4a86aa5b5d5e99be4760c2408f11374628e388
Philani Mxathule <3911755@myuwc.ac.za>
2022-10-18 10:16:44 +0200

    initial commit

---
+++ while.py
@@ -0,0 +1,30 @@
+# i = 1
+# while i<=5:
+#         print("Philani" , i)
+#         i=i+1
+
+# i = 5
+# while i>=1:
+#         print("Philani" , i)
+#         i=i-1
+
+# i = 5
+# j = 1
+# while i>=1:
+#         print("Philani")
+#         while j<=4:
+#             print("none")
+#             j=j+1
+#         i=i-1
+
+
+i = 1
+j = 1
+while i<=5:
+        print("Philani ", end="")
+        j=1
+        while j<=4:
+            print("none ", end="")
+            j=j+1
+        i=i+1
+        print()
